a124e74a94f3f6e21e9557a4b90b36a8c79ddb32
LACOUR Valentin <valentin.lacour@etud.u-picardie.fr>
2016-12-02 15:04:15 +0100

    Ajout du controller "survey_show" + Amélioration et correctifs
    Permet de visualiser un questionnaire vide

---
+++ private/controller/survey/survey_show.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valentinlacour
+ * Date: 02/12/16
+ * Time: 14:24
+ */
+
+$html = new \general\HTML();
+$gabarit = \general\Language::translate_gabarit('pages/admin_survey_show');
+
+$script_vendor = array(
+    'underscore/underscore-min.js',
+    'backbone/backbone.js',
+    'backbone.paginator/lib/backbone.paginator.min.js',
+    'backgrid/lib/backgrid.js',
+    'backgrid-paginator/backgrid-paginator.js',
+    'datatables/media/js/jquery.dataTables.js',
+    'bootstrap-select/bootstrap-select.min.js',
+    'messenger/build/js/messenger.js',
+    'messenger/build/js/messenger-theme-flat.js'
+);
+$script = array('survey_part2.js');
+
+$survey_datas = \app\Answer::get_survey(1);
+$survey_gabarit = \general\Language::translate_gabarit('components/survey_read2');
+$survey = "";
+
+//echo '<pre>';
+//var_dump($survey_datas);
+
+foreach($survey_datas->questions as $key=>$question){
+    $reponses = "";
+    foreach($question as $key2=>$reponse){
+        if(is_a($reponse,'StdClass')){
+            $reponses .= '<div class="radio radio-primary display-inline-block ml-n mr-n"><input type="radio" name="radio2" id="'.$key.$key2.'" value="option1" disabled="disabled"><label for="'.$key.$key2.'">'.$reponse->lbl.'</label></div>';
+        }
+    }
+    $replace = array('{id-survey}','{survey-lbl}','{responses}');
+    $by = array($key+1,$question->questionLbl,$reponses);
+    $survey .= str_replace($replace,$by,$survey_gabarit);
+}
+
+$gabarit = str_replace('{question_list}',$survey,$gabarit);
+
+$html->open();
+$html->sidebar();
+$html->navbar();
+echo $gabarit;
+$html->close($script_vendor,$script);
