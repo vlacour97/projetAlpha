a1173de82301f1013b72479463278a6eadb4802d
LACOUR Valentin <valentin.lacour@etud.u-picardie.fr>
2016-12-04 01:07:59 +0100

    Ajout de la fonctionnalité de traduction des questionnaires
    Gére automatiquement la langue de l'utilisateur et traduit les questionnaire dans sa langue quand il le demande

--- private/controller/answer_survey.php
+++ private/controller/answer_survey.php
@@ -72,11 +72,15 @@ $survey_datas = \app\Answer::get_answers($id);
 $survey_gabarit = \general\Language::translate_gabarit('components/answer_survey2');
 $survey = "";
 
+(\app\Log::get_lang() == mainClass::$lang) && isset($_GET['translate']) && $_GET['translate'] = false;
+
 foreach($survey_datas->questions as $key=>$question){
     $reponses = "";
     $nb_point = 0;
     foreach($question as $key2=>$reponse){
         if(is_a($reponse,'StdClass')){
+            if(isset($_GET['translate']) && $_GET['translate'] == true)
+                $reponse->lbl = \general\Language::translate_text($reponse->lbl,mainClass::$lang,\app\Log::get_lang());
             if($reponse->choice)
                 $reponses .= '<div class="radio radio-primary display-inline-block ml-n mr-n"><input type="radio" name="radio'.$key.'" id="'.$key.$key2.'" value="'.$key2.'" checked="checked"><label for="'.$key.$key2.'">'.$reponse->lbl.'</label></div>';
             else
@@ -94,6 +98,9 @@ foreach($survey_datas->questions as $key=>$question){
         $notHidden = "";
     }
 
+    if(isset($_GET['translate']) && $_GET['translate'] == true)
+        $question->questionLbl = \general\Language::translate_text($question->questionLbl,mainClass::$lang,\app\Log::get_lang());
+
     $replace = array('{id-survey}','{survey-lbl}','{comment}','{responses}','{hidden}','{not-hidden}','{ID}');
     $by = array($key+1,$question->questionLbl,$question->comments,$reponses,$hidden,$notHidden,$key);
     $survey .= str_replace($replace,$by,$survey_gabarit);
@@ -103,8 +110,20 @@ $student = \app\User::get_student($id);
 $ti = \app\User::get_user($student->ID_TI);
 $te = \app\User::get_user($student->ID_TE);
 
-$replace = array('{survey_datas}','{student-id}','{student-fname}','{student-name}','{student-phone}','{student-email}','{ti-fname}','{ti-name}','{ti-phone}','{ti-email}','{te-fname}','{te-name}','{te-phone}','{te-email}','{ID_Student}','{returnPath}');
-$by = array($survey,\general\crypt::encrypt($student->ID),$student->fname,$student->name,$student->phone,$student->email,$ti->fname,$ti->name,$ti->phone,$ti->email,$te->fname,$te->name,$te->phone,$te->email,$_GET['id'],'?'.\app\Navigation::$navigation_marker.'=student_list');
+if(\app\Log::get_lang() == mainClass::$lang)
+    $translate_btn = "";
+else{
+    if((isset($_GET['translate']) && $_GET['translate'] == true)){
+        $translate_btn = \general\Language::translate_gabarit('components/parts/translate_btn_back');
+        $translate_btn = str_replace('{link}','?'.\app\Navigation::$navigation_marker.'='.$_GET[\app\Navigation::$navigation_marker].'&id='.$_GET['id'],$translate_btn);
+    }else{
+        $translate_btn = \general\Language::translate_gabarit('components/parts/translate_btn');
+        $translate_btn = str_replace('{link}','?'.\app\Navigation::$navigation_marker.'='.$_GET[\app\Navigation::$navigation_marker].'&id='.$_GET['id'].'&translate=true',$translate_btn);
+    }
+}
+
+$replace = array('{survey_datas}','{student-id}','{student-fname}','{student-name}','{student-phone}','{student-email}','{ti-fname}','{ti-name}','{ti-phone}','{ti-email}','{te-fname}','{te-name}','{te-phone}','{te-email}','{ID_Student}','{returnPath}','{translate_button}');
+$by = array($survey,\general\crypt::encrypt($student->ID),$student->fname,$student->name,$student->phone,$student->email,$ti->fname,$ti->name,$ti->phone,$ti->email,$te->fname,$te->name,$te->phone,$te->email,$_GET['id'],'?'.\app\Navigation::$navigation_marker.'=student_list',$translate_btn);
 $gabarit = str_replace($replace,$by,$gabarit);
 
 $html->open();
--- private/controller/see_survey.php
+++ private/controller/see_survey.php
@@ -39,17 +39,25 @@ $survey_datas = \app\Answer::get_answers($id);
 $survey_gabarit = \general\Language::translate_gabarit('components/survey_read3');
 $survey = "";
 
+(\app\Log::get_lang() == mainClass::$lang) && isset($_GET['translate']) && $_GET['translate'] = false;
+
 foreach($survey_datas->questions as $key=>$question){
     $reponses = "";
     $nb_point = 0;
     foreach($question as $key2=>$reponse){
         if(is_a($reponse,'StdClass')){
+            if(isset($_GET['translate']) && $_GET['translate'] == true)
+                $reponse->lbl = \general\Language::translate_text($reponse->lbl,mainClass::$lang,\app\Log::get_lang());
             if($reponse->choice)
                 $reponses .= '<div class="radio radio-primary display-inline-block ml-n mr-n"><input type="radio" name="radio2" id="'.$key.$key2.'" value="option1" disabled="disabled" checked="checked"><label for="'.$key.$key2.'">'.$reponse->lbl.'</label></div>';
             else
                 $reponses .= '<div class="radio radio-primary display-inline-block ml-n mr-n"><input type="radio" name="radio2" id="'.$key.$key2.'" value="option1" disabled="disabled"><label for="'.$key.$key2.'">'.$reponse->lbl.'</label></div>';
         }
     }
+
+    if(isset($_GET['translate']) && $_GET['translate'] == true)
+        $question->questionLbl = \general\Language::translate_text($question->questionLbl,mainClass::$lang,\app\Log::get_lang());
+
     $replace = array('{id-survey}','{survey-lbl}','{comment}','{responses}');
     $by = array($key+1,$question->questionLbl,$question->comments,$reponses);
     $survey .= str_replace($replace,$by,$survey_gabarit);
@@ -59,8 +67,21 @@ $student = \app\User::get_student($id);
 $ti = \app\User::get_user($student->ID_TI);
 $te = \app\User::get_user($student->ID_TE);
 
-$replace = array('{survey_datas}','{student-id}','{student-fname}','{student-name}','{student-phone}','{student-email}','{ti-fname}','{ti-name}','{ti-phone}','{ti-email}','{te-fname}','{te-name}','{te-phone}','{te-email}','{validate_link}');
-$by = array($survey,\general\crypt::encrypt($student->ID),$student->fname,$student->name,$student->phone,$student->email,$ti->fname,$ti->name,$ti->phone,$ti->email,$te->fname,$te->name,$te->phone,$te->email,'?nav=see_survey&action=validate&id='.\general\crypt::encrypt($student->ID));
+if(\app\Log::get_lang() == mainClass::$lang)
+    $translate_btn = "";
+
+else{
+    if((isset($_GET['translate']) && $_GET['translate'] == true)){
+        $translate_btn = \general\Language::translate_gabarit('components/parts/translate_btn_back');
+        $translate_btn = str_replace('{link}','?'.\app\Navigation::$navigation_marker.'='.$_GET[\app\Navigation::$navigation_marker].'&id='.$_GET['id'],$translate_btn);
+    }else{
+        $translate_btn = \general\Language::translate_gabarit('components/parts/translate_btn');
+        $translate_btn = str_replace('{link}','?'.\app\Navigation::$navigation_marker.'='.$_GET[\app\Navigation::$navigation_marker].'&id='.$_GET['id'].'&translate=true',$translate_btn);
+    }
+}
+
+$replace = array('{survey_datas}','{student-id}','{student-fname}','{student-name}','{student-phone}','{student-email}','{ti-fname}','{ti-name}','{ti-phone}','{ti-email}','{te-fname}','{te-name}','{te-phone}','{te-email}','{validate_link}','{translate_button}');
+$by = array($survey,\general\crypt::encrypt($student->ID),$student->fname,$student->name,$student->phone,$student->email,$ti->fname,$ti->name,$ti->phone,$ti->email,$te->fname,$te->name,$te->phone,$te->email,'?nav=see_survey&action=validate&id='.\general\crypt::encrypt($student->ID),$translate_btn);
 $gabarit = str_replace($replace,$by,$gabarit);
 
 $html->open();
